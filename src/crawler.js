a2a382a21d961d50012e81cb5eb45d9bfe1f46e0
soo054141 <48131318+soo054141@users.noreply.github.com>
2021-07-19 21:35:21 +0900

    test2

--- src/crawler.js
+++ src/crawler.js
@@ -1,9 +1,9 @@
-// fetch(
-//   "https://stdict.korean.go.kr/api/search.do?key=2ACB7EF719126ADDB05C1EFCAF7E56EC&q=안녕&method=word_info"
-// ).then((response) => console.log(response));
+fetch(
+  "https://stdict.korean.go.kr/api/search.do?key=2ACB7EF719126ADDB05C1EFCAF7E56EC&q=안녕&method=word_info"
+).then((response) => console.log(response));
 
-const request = new Request(
-  "https://stdict.korean.go.kr/api/search.do?key=2ACB7EF719126ADDB05C1EFCAF7E56EC&method=word_info"
-);
+// const request = new Request(
+//   "https://stdict.korean.go.kr/api/search.do?key=2ACB7EF719126ADDB05C1EFCAF7E56EC&method=word_info"
+// );
 
-fetch(request).then((response) => response.blob());
+// fetch(request).then((response) => response.blob());
